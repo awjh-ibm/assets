26393d005916b4171f9ba4f114a49a1ab0966705
Andrew W Hurt <Andrew.Hurt1@ibm.com>
2019-09-02 09:54:03 +0100

    adding brussels to private collections (#19)
    Signed-off-by: Andrew Hurt <andrew.hurt1@ibm.com>

--- src/main/java/com/wetrade/assets/utils/Constants.java
+++ src/main/java/com/wetrade/assets/utils/Constants.java
@@ -1,5 +1,5 @@
 package com.wetrade.assets.utils;
 
 public class Constants {
-    final public static String PRIVATE_COLLECTIONS = "AnyOf('AmsterdamBank')"; // TODO MAKE IT DO ALL THE COLLECTIONS
+    final public static String PRIVATE_COLLECTIONS = "AnyOf('AmsterdamBank', 'BankOfBrussels')"; // TODO MAKE IT DO ALL THE COLLECTIONS
 }
